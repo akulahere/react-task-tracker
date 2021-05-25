961583114890c69ec6c6d04f709a7f83ddc925c5
dmitry <akulinind@gmail.com>
2021-05-25 14:09:34 +0300

    feat: implement task component, delete task feature, reminder logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import  { useState } from 'react';
 
 import './App.css';
@@ -6,11 +5,25 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
-  const [tasks, setTasks] = useState([{id: 1, text: 111}, {id: 2, text: 222}]);
+  const [tasks, setTasks] = useState([
+    {id: 1, day: 'one', text: 111, reminder: true},
+    {id: 2, day: 'two', text: 222, reminder: true}]);
+  //delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task));
+  }
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ?
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+        : 'No tasks To Show'
+      }
     </div>
   )
 }
