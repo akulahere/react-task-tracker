e69732440b4e575355326c1f0bd174ee5e96bc6c
dmitry <akulinind@gmail.com>
2021-05-25 13:33:59 +0300

    feat: implement header, button, tasks components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
+import  { useState } from 'react';
+
 import './App.css';
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
 function App() {
-  const name ='Ivan';
+  const [tasks, setTasks] = useState([{id: 1, text: 111}, {id: 2, text: 222}]);
   return (
-    <div className={App}>
+    <div className='container'>
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
+import Button from "./Button";
 
 const Header = ({ title }) => {
+  const onClick = () => console.log('click');
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
+      <Button   text='Add' />
     </header>
   )
 }
